5b427c6ebd85f78a0883ee933253cd225f96b26a
Andi <coconuthunted69@gmail.com>
2024-10-21 10:35:13 +0300

    Update message error

--- src/App.jsx
+++ src/App.jsx
@@ -104,7 +104,10 @@ const App = () => {
           />
         </label>
         {error && (
-          <div className="text-red-500 text-sm mb-4">{error}</div>
+          <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
+            <strong className="font-bold">Erreur: </strong>
+            <span className="block sm:inline">{error}</span>
+          </div>
         )}
         <button
           onClick={handlePredictClick}
